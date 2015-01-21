987268dfc921fdfdeba84692769f0944626b8656
Adam Luckenbaugh <adam.luckenbaugh@gmail.com>
2015-01-21 11:46:24 -0500

    Initial commit.

---
+++ httpserver.c
@@ -0,0 +1,13 @@
+#ifndef HTTP_SERVER_H
+#define HTTP_SERVER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[])
+{
+  printf("HTTP Server startup.\n");
+  return 0;
+}
+
+#endif

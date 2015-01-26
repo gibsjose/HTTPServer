61e24b266c62c6597ac8ca582ba97ece4bf352c0
Adam Luckenbaugh <adam.luckenbaugh@gmail.com>
2015-01-26 17:23:49 -0500

    Fixed the threading for each client socket; responsibly handle the CTRL+C signal and clean up the server socket; added error checking code.

--- requesthandler.c
+++ requesthandler.c
@@ -4,13 +4,66 @@ void * requesthandler_run(void * aData_ptr)
 {
   //Dereference the void* pointer to be an int*.
   int * lSocketFD = (int *) aData_ptr;
-  char line[5000];
+  char line[SOCKET_BUFFER_BYTES];
 
-  recv(*lSocketFD,line,5000,0);
-  send(*lSocketFD,line,strlen(line),0);
-  printf("Got from client: %s\n", line);
+  // Set a read timeout on the socket.
+  struct timeval timeout;
+  timeout.tv_sec = SOCKET_READ_TIMEOUT_SEC;
+  timeout.tv_usec = 0;
 
-  close(*lSocketFD);
+  setsockopt(*lSocketFD, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout,sizeof(struct timeval));
 
-  return NULL;
+  while(1)
+  {
+    printf("Receiving...\n");
+    size_t lRetVal_recv = recv(*lSocketFD, line, SOCKET_BUFFER_BYTES, 0);
+    if (0 == lRetVal_recv)
+    {
+      // Client likely closed the connection.
+      pthread_exit(NULL);
+    }
+    else if(-1 == lRetVal_recv)
+    {
+      //some other error
+      fprintf(stderr, "recv(): Bad doodoo.\n");
+      perror(strerror(errno));
+      pthread_exit(NULL);
+    }
+    else
+    {
+      printf("Received %lu bytes.\n", lRetVal_recv);
+    }
+
+    printf("Got from client:\n");
+    printf("-----------------------------\n");
+    printf("%s\n", line);
+    printf("-----------------------------\n");
+
+    // TODO: Change me.  I just send back the same data I was sent.
+    printf("Sending...\n");
+    size_t lRetVal_send = send(*lSocketFD,line,strlen(line),0);
+    if(-1 == lRetVal_send)
+    {
+      fprintf(stderr, "send(): Bad doodoo.\n");
+      perror(strerror(errno));
+      pthread_exit(NULL);
+    }
+    else
+    {
+      printf("Sent %lu bytes.\n", lRetVal_send);
+    }
+
+    /*
+     * In HTTP 1.1 all connections are considered persistent unless declared otherwise.
+     * http://en.wikipedia.org/wiki/HTTP_persistent_connection
+     * So close the socket if the Connection: close header is sent.
+     */
+
+    // TODO: Change me to intelligently close and exit.
+    close(*lSocketFD);
+    pthread_exit(0);
+
+    // Clear the buffer for reading from the client socket.
+    memset(&line, 0, SOCKET_BUFFER_BYTES);
+  }
 }
--- requesthandler.h
+++ requesthandler.h
@@ -1,11 +1,18 @@
 #ifndef REQUEST_HANDLER_H
 #define REQUEST_HANDLER_H
 
+#define SOCKET_READ_TIMEOUT_SEC 20
+#define SOCKET_BUFFER_BYTES 50000
+
+#include <errno.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/time.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <pthread.h>
 
 void * requesthandler_run(void * aData_ptr);
 

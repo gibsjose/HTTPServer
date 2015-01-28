7db4a2144b0be5fee114bfcc768e884f07871da9
Adam Luckenbaugh <adam.luckenbaugh@gmail.com>
2015-01-28 09:59:46 -0500

    Updated the launch script to point to the www directory.

--- launchserver.sh
+++ launchserver.sh
@@ -1,5 +1,5 @@
 # Launch with no log file (log messages will be printed on stdout only).
-#./httpserver -p 8080 -docroot ./
+#./httpserver -p 8080 -docroot ./www/
 
 # Launch with a log file.
-./httpserver -p 8080 -docroot ./ -logfile httpserver.log
+./httpserver -p 8080 -docroot ./www/ -logfile httpserver.log
